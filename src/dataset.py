c4feded8093b1745669ddbeece1d27cfdb9f9cb7
Toshiba Mastru <ggostem42@gmail.com>
2025-05-30 16:11:44 +0300

    improved dataset

--- src/dataset.py
+++ src/dataset.py
@@ -1,19 +1,18 @@
+import numpy as np
 import pandas as pd
 import torch
 from torch.utils.data import Dataset
 
-from .utils import scale
+from .utils import scale_robust
 
 
 class TradeDataset(Dataset):
-    def __init__(self, path: str, seq_len: int, pred_len: int):
-        df = pd.read_pickle(path)[-50_000:-4_000]  # [-500_000:-4_000]
-        df = df[~df.index.duplicated(keep="first")]
-
+    def __init__(self, df: pd.DataFrame, seq_len: int, pred_len: int):
         self.seq_len = seq_len
         self.pred_len = pred_len
 
-        self.data = df[["close", "high", "low", "volume"]]
+        self.data = df[["close", "high", "low", "volume"]].values
+        self.feats = self._gen_time_features(df.index)
 
     def __len__(self):
         return len(self.data) - self.seq_len - self.pred_len + 1
@@ -26,13 +25,26 @@ class TradeDataset(Dataset):
         input_seq = self.data[s_begin:s_end]
         target_seq = self.data[s_end:r_end]
 
-        price, pmn, pmx = scale(input_seq[["close", "high", "low"]])
-        volume, vmn, vmx = scale(input_seq[["volume"]])
+        price, pmn, pmx = scale_robust(input_seq[:, 0:3])
+        volume, vmn, vmx = scale_robust(input_seq[:, 3:])
 
-        tprice, _, _ = scale(target_seq[["close", "high", "low"]], pmn, pmx)
-        tvolume, _, _ = scale(target_seq[["volume"]], vmn, vmx)
+        tprice, _, _ = scale_robust(target_seq[:, 0:3], pmn, pmx)
+        tvolume, _, _ = scale_robust(target_seq[:, 3:], vmn, vmx)
 
-        x = pd.concat([price, volume], axis=1).values
-        y = pd.concat([tprice, tvolume], axis=1).values
+        x = np.concat([price, volume], axis=1)
+        y = np.concat([tprice, tvolume], axis=1)
+        mark = self.feats[s_begin:r_end]
 
-        return (torch.FloatTensor(x), torch.FloatTensor(y))
+        return (
+            torch.FloatTensor(x),
+            torch.FloatTensor(y),
+            torch.FloatTensor(mark)
+        )
+
+    @staticmethod
+    def _gen_time_features(dates: pd.Index) -> np.ndarray:
+        months = dates.month.values
+        days = dates.day.values
+        weekdays = dates.weekday.values
+        hours = dates.hour.values
+        return np.column_stack([months, days, weekdays, hours])
